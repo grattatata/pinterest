3e3536e0caa3a1b551e5e28ed6986823d65bb252
damiiya <ddam2lee@gmail.com>
2022-06-18 16:23:25 +0900

    Header.js 와 Login modal 이슈 있습니다.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../elements/Input";
+import ButtonEle from "../elements/ButtonEle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPinterest } from "@fortawesome/free-brands-svg-icons";
 
-const Login = () => {
-  return <div>Login</div>;
+const LoginModal = ({ closeModal }) => {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="modal-header" style={{ height: "150px" }}>
+          <FontAwesomeIcon
+            style={{
+              fontSize: "30px",
+              color: "#E60B23",
+            }}
+            icon={faPinterest}
+          />
+          <h1 className="login-title">
+            Pinterest에 오신 것을
+            <br /> 환영합니다
+          </h1>
+        </div>
+        <form className="login-form">
+          <label>이메일</label>
+          <Input placeholder="이메일" />
+          <label>비밀번호</label>
+          <Input placeholder="비밀번호" />
+        </form>
+
+        <ButtonEle backgroundColor="#E60B23" text="로그인" />
+        <button
+          onClick={() => {
+            closeModal(false);
+          }}
+        >
+          Close Modal
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default Login;
+export default LoginModal;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import Input from "../elements/Input";
+import ButtonEle from "../elements/ButtonEle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPinterest } from "@fortawesome/free-brands-svg-icons";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="modal wrap">
+      <div className="modal-overlay">
+        <div className="modal-content">
+          <div className="modal-header" style={{ height: "150px" }}>
+            <FontAwesomeIcon
+              style={{
+                fontSize: "30px",
+                color: "#E60B23",
+              }}
+              icon={faPinterest}
+            />
+            <h1 className="login-title">
+              Pinterest에 오신 것을
+              <br /> 환영합니다
+            </h1>
+          </div>
+          <form className="login-form">
+            <label>이메일</label>
+            <Input placeholder="이메일" />
+            <label>비밀번호</label>
+            <Input placeholder="비밀번호" />
+          </form>
+
+          <ButtonEle backgroundColor="#E60B23" text="로그인" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
